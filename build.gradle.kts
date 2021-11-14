5a1d07533a6cc3f634fc5c15aaa5efcc9e230d9c
ForteScarlet <ForteScarlet@163.com>
2021-11-14 23:57:48 +0800

    nexus plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     java
     id("maven-publish")
     id("signing")
+    id("io.codearte.nexus-staging") version "0.22.0" apply false
+
 }
 
 group = P.GROUP // "love.forte.annotationTool"
@@ -45,63 +47,3 @@ subprojects {
     }
 }
 
-
-// inline fun Project.configurePublishing(
-//     artifactId: String,
-//     vcs: String = "https://github.com/ForteScarlet/annotation-tool",
-// ) {
-//     // configureRemoteRepos()
-//     // apply<ShadowPlugin>()
-//
-//
-//     val sourcesJar by tasks.registering(Jar::class) {
-//         archiveClassifier.set("sources")
-//         from(sourceSets["main"].allSource)
-//     }
-//     // val sourcesJar = tasks["sourcesJar"]
-//     val javadocJar = tasks.register("javadocJar", Jar::class) {
-//         @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
-//         archiveClassifier.set("javadoc")
-//     }
-//
-//     publishing {
-//         publications {
-//             register("mavenJava", MavenPublication::class) {
-//                 from(components["java"])
-//
-//                 groupId = rootProject.group.toString()
-//                 setArtifactId(artifactId)
-//                 version = project.version.toString()
-//
-//                 setupPom(
-//                     project = project,
-//                     vcs = vcs
-//                 )
-//
-//                 artifact(sourcesJar)
-//                 artifact(javadocJar.get())
-//             }
-//         }
-//
-//         repositories {
-//             maven {
-//                 if (version.toString().endsWith("SNAPSHOTS", true)) {
-//                     // snapshot
-//                     name = "snapshots-oss"
-//                     url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-//                 } else {
-//                     name = "oss"
-//                     url = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
-//                 }
-//                 credentials {
-//                     username = project.extra.properties["sonatype.username"]?.toString()
-//                         ?: throw NullPointerException("snapshots-sonatype-username")
-//                     password = project.extra.properties["sonatype.password"]?.toString()
-//                         ?: throw NullPointerException("snapshots-sonatype-password")
-//                 }
-//             }
-//         }
-//
-//         // configGpgSign(this@configurePublishing)
-//     }
-// }
